e8842aae9f4196ca625e83dcd770d52345271032
Thomas King <tking.91@gmail.com>
2017-03-01 11:12:07 -0500

    Changes to DetectorDriverXmlParser
    These changes used to be in DetectorDiver.cpp but with the upstream changes they go here now

--- Analysis/Utkscan/core/source/DetectorDriverXmlParser.cpp
+++ Analysis/Utkscan/core/source/DetectorDriverXmlParser.cpp
@@ -48,6 +48,7 @@
 #ifdef useroot //Some processors REQUIRE ROOT to function
 #include "Anl1471Processor.hpp"
 #include "IS600Processor.hpp"
+#include "Ornl2016Processor.hpp"
 #include "RootProcessor.hpp"
 #include "TwoChanTimingProcessor.hpp"
 #endif
@@ -82,6 +83,13 @@ vector<EventProcessor*> DetectorDriverXmlParser::ParseProcessors(
         string name = processor.attribute("name").as_string();
 
         messenger_.detail("Loading " + name);
+
+        //LaBr and NaI subEventWindows and Thresholds are slaved to the clover by default
+        // moving declaration here for that reason
+
+        double gamma_threshold=0;
+        double sub_event=0;
+
         if (name == "BetaScintProcessor") {
             vecProcess.push_back(new BetaScintProcessor(
                     processor.attribute("gamma_beta_limit").as_double(200.e-9),
@@ -89,11 +97,11 @@ vector<EventProcessor*> DetectorDriverXmlParser::ParseProcessors(
         } else if (name == "GeProcessor") {
             ///@TODO This needs to be cleaned. No method should have this
             /// many variables as arguments.
-            vecProcess.push_back(new GeProcessor(
-                    processor.attribute("gamma_threshold").as_double(1.0),
+             vecProcess.push_back(new GeProcessor(
+                    gamma_threshold = processor.attribute("gamma_threshold").as_double(1.0),
                     processor.attribute("low_ratio").as_double(1.0),
                     processor.attribute("high_ratio").as_double(3.0),
-                    processor.attribute("sub_event").as_double(100.e-9),
+                    sub_event = processor.attribute("sub_event").as_double(100.e-9),
                     processor.attribute("gamma_beta_limit").as_double(200.e-9),
                     processor.attribute("gamma_gamma_limit").as_double(200.e-9),
                     processor.attribute("cycle_gate1_min").as_double(0.0),
@@ -148,7 +156,31 @@ vector<EventProcessor*> DetectorDriverXmlParser::ParseProcessors(
             vecProcess.push_back(new VandleOrnl2012Processor());
         } else if (name == "RootProcessor") {
             vecProcess.push_back(new RootProcessor("tree.root", "tree"));
+        }else if (name == "Ornl2016Processor") {
+            double gamma_threshold_G = gamma_threshold;
+            double sub_event_G = sub_event;
+
+            double gamma_threshold_L =
+                    processor.attribute("gamma_threshold_L").as_double(gamma_threshold_G);
+            if (gamma_threshold_L == gamma_threshold_G)
+                messenger_.warning("LaBr3 Threshold = Ge Threshold", 1);
+            double sub_event_L =
+                    processor.attribute("sub_event_L").as_double(sub_event_G);
+            if (sub_event_L == sub_event_G)
+                messenger_.warning("LaBr3 sub_event = Ge sub_event", 1);
+            double gamma_threshold_N =
+                    processor.attribute("gamma_threshold_N").as_double(gamma_threshold_G);
+            if (gamma_threshold_N == gamma_threshold_G)
+                messenger_.warning("NaI Threshold = Ge Threshold", 1);
+            double sub_event_N =
+                    processor.attribute("sub_event_N").as_double(sub_event_G);
+            if (sub_event_N == sub_event_G)
+                messenger_.warning("NaI sub_event = Ge sub_event", 1);
+            vecProcess.push_back(new Ornl2016Processor(gamma_threshold_L, sub_event_L,
+                                                       gamma_threshold_N, sub_event_N,
+                                                       gamma_threshold_G, sub_event_G));
         }
+
 #endif
         else {
             stringstream ss;
