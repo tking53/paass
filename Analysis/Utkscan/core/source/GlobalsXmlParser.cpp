750bc8b8d2025088af992ef0d4bb5323a040cd12
Unknown <tking.91@gmail.com>
2017-04-03 12:54:27 -0400

    Fixing regression in multi Trace Delays
    These 2 changes were lost in a rebase of the branch somehow.

--- Analysis/Utkscan/core/source/GlobalsXmlParser.cpp
+++ Analysis/Utkscan/core/source/GlobalsXmlParser.cpp
@@ -292,6 +292,7 @@ void GlobalsXmlParser::ParseTraceNode(const pugi::xml_node &node, Globals *globa
     } else
         throw CriticalNodeMessage(node.child("TraceDelay").name());
 
+    sstream_.str("");
     if (!node.child("QdcCompression").empty())
         globals->SetQdcCompression(
                 node.child("QdcCompression").attribute("value").as_double());
